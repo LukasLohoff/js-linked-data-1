9df1b662344e9921394848474f6a1e282902f848
Matthias Mohr <m_mohr08@uni-muenster.de>
2015-12-09 17:12:22 +0100

    Formatted the code and some comments.

--- webpage/main.js
+++ webpage/main.js
@@ -1,6 +1,6 @@
 // Map config
-var cityCenter = [51.960942, 7.625891];
-var zoomLevel = 11;
+var cityCenter = [51.960942, 7.625891]; // Coords of the city center of Münster
+var zoomLevel = 11; // Initial zoom level of Münster
 
 // HTML config
 var yearSliderId = "#year-slider";
@@ -11,7 +11,7 @@ var diagramId = "#diagram";
 // JS config
 var map;
 
-$(function() {
+$(function () {
 	// Init map
 	map = L.map(mapId).setView(cityCenter, zoomLevel);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -25,51 +25,51 @@ $(function() {
 		min: 2005,
 		max: 2015,
 		step: 1,
-		slide: function( event, ui ) {
-			$(yearValue).text( ui.value );
+		slide: function (event, ui) {
+			$(yearValue).text(ui.value);
 		}
 	});
-	$(yearValue).text( $(yearSliderId).slider("value") );
-	
+	$(yearValue).text($(yearSliderId).slider("value"));
+
 	// Init diagram
-    $(diagramId).highcharts({
-        title: {
-            text: 'Unemployment in Muenster',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: Stadt Muenster',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Number of Persons'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '# Unit'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Graph One',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'Graph Two',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }]
-    });
+	$(diagramId).highcharts({
+		title: {
+			text: 'Unemployment in Muenster',
+			x: -20 //center
+		},
+		subtitle: {
+			text: 'Source: Stadt Muenster',
+			x: -20
+		},
+		xAxis: {
+			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+				'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+		},
+		yAxis: {
+			title: {
+				text: 'Number of Persons'
+			},
+			plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
+				}]
+		},
+		tooltip: {
+			valueSuffix: '# Unit'
+		},
+		legend: {
+			layout: 'vertical',
+			align: 'right',
+			verticalAlign: 'middle',
+			borderWidth: 0
+		},
+		series: [{
+				name: 'Graph One',
+				data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+			}, {
+				name: 'Graph Two',
+				data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+			}]
+	});
 });
