24829fc26b1e1f689b2fcc67f494b2ed71c73021
Matthias M <webmaster@mamo-net.de>
2015-12-29 02:48:35 +0100

    Chart/diagram is working and shows meaningful data, removed "all years" option, other minor changed.

--- webpage/main.js
+++ webpage/main.js
@@ -7,7 +7,6 @@ var yearSliderId = "#year-slider";
 var yearValueId = "#year";
 var mapIdn = "map";
 var diagramId = "#diagram";
-var yearActiveId = "#active_year";
 var criteriaId = "#criteria";
 var districtId = "#bound_districts";
 var boroughId = "#bound_boroughs";
@@ -22,12 +21,31 @@ var sparqlUrl = "http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?out
 var minYear = 2010;
 var maxYear = 2014;
 
+// SPARQL
+var sqlPrefixes = "\
+prefix dc: <http://purl.org/dc/elements/1.1/>\n\
+prefix geo: <http://www.opengis.net/ont/geosparql#>\n\
+prefix lodcom: <http://vocab.lodcom.de/>\n\
+prefix dbpedia: <http://dbpedia.org/page/classes#>\n\
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n";
+
+// Precalculating data
+var years = [];
+for (var y = minYear; y <= maxYear; y++) {
+	years.push(y);
+}
+
+// Initializing webpage (onload)
 $(function () {
 	// Init map
 	map = L.map(mapIdn).setView(cityCenter, zoomLevel);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
+	// When the popup opens load the chart
+	map.on('popupopen', function () {
+		requestChartData($('#diagram').data('name'));
+	});
 
 	// Init year slider
 	$(yearSliderId).slider({
@@ -35,60 +53,80 @@ $(function () {
 		min: minYear,
 		max: maxYear,
 		step: 1,
-		slide: function () {
-			$(yearActiveId).prop("checked", true); // When sliding the year bar, then unset the "all years" selection
-			updateUI();
+		slide: function (event, ui) {
+			$(yearValueId).text(ui.value);
+			requestMapData();
 		}
 	});
-	$(yearActiveId).change(updateUI);
-	$(districtId).change(updateUI);
-	$(boroughId).change(updateUI);
-	$(criteriaId).change(updateUI);
+	$(districtId).change(requestMapData);
+	$(boroughId).change(requestMapData);
+	$(criteriaId).change(requestMapData);
 
-	updateUI();
+	$(yearValueId).text($(yearSliderId).slider("value"));
+	requestMapData();
 });
 
-function updateUI() {
-	updateYearValueUI();
-	requestData();
-}
-
-function updateYearValueUI() {
-	if ($(yearActiveId).prop("checked")) {
-		$(yearValueId).text($(yearSliderId).slider("value"));
-	}
-	else {
-		$(yearValueId).text("All");
-	}
-}
-
-function requestData() {
-	var query = buildQuery();
+function askTripleStore(query, callbackSuccess) {
 	var url = sparqlUrl + encodeURIComponent(query); // encodeURI is not enough as it doesn't enocde # for example.
-	console.log(query);
-	console.log(url);
+	console.log(query, url);
 	$.ajax({
-	  dataType: "jsonp",
-	  url: url,
-	  success: updateData
+		dataType: "jsonp",
+		url: url,
+		success: callbackSuccess
 	});
 }
 
-function buildQuery() {
-	var yearSelected = $(yearActiveId).prop("checked");
-	var yearValue = $(yearSliderId).slider("value");
-	var boroughSelected = $(boroughId).prop("checked");
-	var criteriaValue = $(criteriaId).val();
-	
-	var sqlArea = boroughSelected ? "dbpedia:borough" : "dbpedia:city_district";
+function requestMapData() {
+	askTripleStore(buildQuerySingleYear(), updateData);
+}
 
-	var sqlCriteria = "?unknown";
+function requestChartData(areaName) {
+	askTripleStore(buildQueryAllYears(areaName), function (data) {
+		updateChart(areaName, data);
+	});
+}
+
+function getCurrentCriteriaName() {
+	var criteriaValue = $(criteriaId).val();
+	switch (criteriaValue) {
+		case "Male":
+		case "Female":
+			return "by gender: " + criteriaValue;
+		case "Longtime":
+			return "by duration: Long-time unemployment";
+		case "Foreign":
+			return "by origin: Foreigners";
+		case "Olderthan55":
+			return "by age: Older than 55 years";
+		case "Youngerthan25":
+			return "by age: Younger than 25 years";
+		case "Between25and55":
+			return "by age: 25 to 55 years";
+		default:
+			return "";
+	}
+}
+
+function buildQuerySingleYear() {
+	var yearValue = $(yearSliderId).slider("value");
+	if (yearValue >= minYear && yearValue <= maxYear) {
+		yearValue = maxYear;
+	}
+
+	var sqlArea = $(boroughId).prop("checked") ? "dbpedia:borough" : "dbpedia:city_district";
+
+	var criteriaValue = $(criteriaId).val();
+
+	var sqlCriteria = "";
+	var sqlValue = "?value";
 	if (criteriaValue == "Between25and55") {
 		// We need to calculate this from other data
+		sqlValue = "(xsd:integer(?valueTotal)-xsd:integer(?valueYounger)-xsd:integer(?valueOlder) AS ?value)";
+		sqlCriteria = "?id lodcom:hasYoungerthan25Unemployment" + yearValue + " ?valueYounger. ?id lodcom:hasOlderthan55Unemployment" + yearValue + " ?valueOlder. ?id lodcom:hasUnemployment" + yearValue + " ?valueTotal.";
 	}
-	else if (yearSelected && yearValue >= minYear && yearValue <= maxYear) {
-		sqlCriteria = "lodcom:has";
-		switch(criteriaValue) {
+	else {
+		sqlCriteria = "?id lodcom:has";
+		switch (criteriaValue) {
 			case "Male":
 			case "Female":
 			case "Longtime":
@@ -98,35 +136,71 @@ function buildQuery() {
 				// Can be added as is
 				sqlCriteria += criteriaValue;
 				break;
-			// default: Nothing to add
+				// default: Nothing to add
 		}
-		sqlCriteria += "Unemployment" + yearValue;
+		sqlCriteria += "Unemployment" + yearValue + " ?value.";
 	}
-	else { // No valid year
-		// Summierung über die Jahre
-	}
-	
-	var query = "\
-	prefix dc: <http://purl.org/dc/elements/1.1/>\r\n\
-	prefix geo: <http://www.opengis.net/ont/geosparql#>\r\n\
-	prefix lodcom: <http://vocab.lodcom.de/>\r\n\
-	prefix dbpedia: <http://dbpedia.org/page/classes#>\r\n\
-	SELECT DISTINCT ?name ?value ?geo\r\n\
-	WHERE { GRAPH <http://course.introlinkeddata.org/G1> {\r\n\
-	   ?id lodcom:TypeofCityDivision " + sqlArea + ".\r\n\
-	   ?id dc:title ?name.\r\n\
-	   ?id dc:coverage ?coverageId.\r\n\
-	   ?coverageId geo:asWKT ?geo.\r\n\
-	   ?id " + sqlCriteria + " ?value.\r\n\
+
+	var query = sqlPrefixes + "\
+	SELECT DISTINCT ?name " + sqlValue + " ?geo\n\
+	WHERE { GRAPH <http://course.introlinkeddata.org/G1> {\n\
+	   ?id lodcom:TypeofCityDivision " + sqlArea + ".\n\
+	   ?id dc:title ?name.\n\
+	   ?id dc:coverage ?coverageId. ?coverageId geo:asWKT ?geo.\n\
+	   " + sqlCriteria + "\n\
 	}}";
-		
+
+	return query;
+}
+
+function buildQueryAllYears(areaName) {
+	var sqlArea = $(boroughId).prop("checked") ? "dbpedia:borough" : "dbpedia:city_district";
+
+	var criteriaValue = $(criteriaId).val();
+	var sqlCriteria = [];
+	var sqlValue = [];
+
+	for (var ix in years) {
+		var year = years[ix];
+		if (criteriaValue == "Between25and55") {
+			// We need to calculate this from other data
+			sqlValue.push("(xsd:integer(?" + year + "Total)-xsd:integer(?" + year + "Younger)-xsd:integer(?" + year + "Older) AS ?" + year + ")");
+			sqlCriteria.push("?id lodcom:hasYoungerthan25Unemployment" + year + " ?" + year + "Younger. ?id lodcom:hasOlderthan55Unemployment" + year + " ?" + year + "Older. ?id lodcom:hasUnemployment" + year + " ?" + year + "Total.");
+		}
+		else {
+			var sqlTemp = "?id lodcom:has";
+			switch (criteriaValue) {
+				case "Male":
+				case "Female":
+				case "Longtime":
+				case "Foreign":
+				case "Olderthan55":
+				case "Youngerthan25":
+					// Can be added as is
+					sqlTemp += criteriaValue;
+					break;
+					// default: Nothing to add
+			}
+			sqlTemp += "Unemployment" + year + " ?" + year + ".";
+			sqlCriteria.push(sqlTemp);
+			sqlValue.push("?" + year);
+		}
+	}
+
+	var query = sqlPrefixes + "\
+	SELECT DISTINCT " + sqlValue.join(" ") + "\n\
+	WHERE { GRAPH <http://course.introlinkeddata.org/G1> {\n\
+	   ?id lodcom:TypeofCityDivision " + sqlArea + ".\n\
+	   ?id dc:title \"" + areaName + "\".\n\
+	   " + sqlCriteria.join(" ") + "\n\
+	}}";
+
 	return query;
 }
 
 function updateData(data) {
 	// Remove old data from map
-	for(var row in features){
-		console.log(features[row]);
+	for (var row in features) {
 		if (features.hasOwnProperty(row)) {
 			map.removeLayer(features[row]);
 		}
@@ -136,17 +210,23 @@ function updateData(data) {
 	// Work with the data
 	var bindings = data.results.bindings;
 	var defaultOptions = map.defaults || {};
-    for(var row in bindings){
+	for (var row in bindings) {
+		var value = null;
 		// Get the data
 		var name = bindings[row].name.value;
-		var value = bindings[row].value.value;
+		if (bindings[row].value) {
+			value = bindings[row].value.value;
+		}
 		var geo = bindings[row].geo.value;
 		// Remove the CRS url from the string. Can't be parsed and Leaflet is in EPSG4326 anyway.
 		geo = geo.replace("<http://www.opengis.net/def/crs/EPSG/0/4326>", "")
 		// Read WKT and create leaflet object
 		wkt.read(geo);
 		// Change color according to data
-		if (value < 100) {
+		if (value == null || value < 0) {
+			defaultOptions.color = "#000000";
+		}
+		else if (value < 100) {
 			defaultOptions.color = "#006622";
 		}
 		else if (value < 1000) {
@@ -157,53 +237,63 @@ function updateData(data) {
 		}
 		var obj = wkt.toObject(defaultOptions);
 		// Bind popup with additional data
-		obj.bindPopup("<strong>" + name + "</strong><br />" + "Unemployment count: " + value);
+		obj.bindPopup(
+				"<div id='diagram' data-name=\"" + name + "\">No chart available so far.</div>",
+				{maxWidth: 450}
+		);
 		// Add object to map and feature array (for later removal)
 		map.addControl(obj);
 		features.push(obj);
-    }
-	
-	updateChart();
+	}
+
+	updateChart(null);
 }
 
-function updateChart() {
-	$(diagramId).highcharts({
-		title: {
-			text: 'Unemployment in Muenster',
-			x: -20 //center
-		},
-		subtitle: {
-			text: 'Source: Stadt Muenster',
-			x: -20
-		},
-		xAxis: {
-			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		},
-		yAxis: {
+function updateChart(areaName, data) {
+	if (areaName == null || !Array.isArray(data.results.bindings)) {
+		$(diagramId).text("No data available.");
+	}
+	else {
+		var dataSeries1 = [];
+		var bindings = data.results.bindings.shift();
+		for (var key in bindings) {
+			if (bindings[key].value) {
+				// We hope that the values are in ascending order, like the query has been created.
+				// Not very elegant, but I dont see a better solution so far and it should work theoretically.
+				dataSeries1.push(parseInt(bindings[key].value, 10));
+			}
+		}
+		var yearIndex = $(yearSliderId).slider("value") - minYear;
+		$(diagramId).highcharts({
 			title: {
-				text: 'Number of Persons'
+				text: 'Unemployment in Münster ' + areaName,
+				style: { fontSize: '16px' }
 			},
-			plotLines: [{
-					value: 0,
-					width: 1,
-					color: '#808080'
+			subtitle: {
+				text: getCurrentCriteriaName(),
+				style: { fontSize: '13px' }
+			},
+			credits: {
+				href: "http://www.muenster.de/stadt/stadtplanung/zahlen.html",
+				text: "Source: Stadt Muenster"
+			},
+			xAxis: {
+				title: {text: 'Year'},
+				categories: years,
+				plotBands: [{
+						color: '#FCFCE5',
+						from: yearIndex - 0.5,
+						to: yearIndex + 0.5,
+					}]
+			},
+			yAxis: {
+				title: {text: 'Number of unemployed citizens'},
+			},
+			legend: {enabled: false},
+			series: [{
+					name: areaName,
+					data: dataSeries1
 				}]
-		},
-		tooltip: {
-			valueSuffix: '# Unit'
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle',
-			borderWidth: 0
-		},
-		series: [{
-				name: 'Graph One',
-				data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-			}, {
-				name: 'Graph Two',
-				data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-			}]
-	});
+		});
+	}
 }
